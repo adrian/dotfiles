9acd927716366f72d3f8e5f8ae80284f3fd466d9
Adrian Smith <adrian@17od.com>
2013-07-14 20:11:44 +0100

    New version of make-links.sh to handle dirs
    This new version takes care of linking to files within dot directories
    in the home directory, e.g. .config

--- make-links.sh
+++ make-links.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
-# Make links for all dot-files found in $HOME/dotfiles
-# ** Will not remove any non-links found in $HOME **
 
-# Source: https://github.com/lad/dotfiles
+SOURCE_DIR=$(dirname $0)
+FILES=$(find $SOURCE_DIR -regex './\..*' -type f -not -path '*.git/*')
+TARGET_PATH=$HOME
 
-curdir="$PWD"
-dotfiles_dir=$(readlink -f $(dirname $0))
+for SOURCE_FILE in $FILES; do
+    TARGET_FILE=$TARGET_PATH/$SOURCE_FILE
 
-cd $dotfiles_dir
-files=$(ls -ad .[A-z]* | egrep -v '.git$|.*.swp')
-
-cd $HOME
-for f in ${files[@]}; do
-    fullf="${dotfiles_dir}/$f"
-
-    printf "\e[1;33m%s\e[0m " $f
-    if [ -e $f -a ! -L $f ]; then
+    printf "\e[1;33m%s\e[0m " $SOURCE_FILE
+    if [ -e $TARGET_FILE -a ! -h $TARGET_FILE ]; then
         printf "exists and is not a link (\e[1;31mnot removed\e[0m)\n"
     else
-        \rm -f $f
-        ln -s $fullf $f
-        printf -- "-> \e[1;32mdotfiles/$f\e[0m done\n"
+        mkdir -p $(dirname $TARGET_FILE)
+        rm -f $TARGET_FILE
+        ln -s $(readlink -e $SOURCE_FILE) $TARGET_FILE
+        printf -- "-> \e[1;32m$TARGET_FILE\e[0m done\n"
     fi
 done
-
-cd $curdir
